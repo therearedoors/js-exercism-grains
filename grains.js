c6f1f6c31f411929576aeba1cbc8f601f383e40a
Paul Smart <pdsmart6@gmail.com>
2021-12-14 15:31:26 +0000

    grains exercise complete

--- grains.js
+++ grains.js
@@ -1,13 +1,13 @@
-// console.log("There once was a wise servant who saved the life of a prince. The king promised to pay whatever the servant could dream up. Knowing that the king loved chess, the servant told the king he would like to have grains of wheat. One grain on the first square of a chess board, with the number of grains doubling on each successive square.")
+console.log("There once was a wise servant who saved the life of a prince. The king promised to pay whatever the servant could dream up. Knowing that the king loved chess, the servant told the king he would like to have grains of wheat. One grain on the first square of a chess board, with the number of grains doubling on each successive square.");
 const grainCount = function(startingGrains,totalNumOfSquares){
     let countSquares = 1;
     let totalNumOfGrains = startingGrains
+    console.log(`Square ${countSquares} equals ${startingGrains} grains.`)
     while (countSquares<totalNumOfSquares) {
-        console.log(startingGrains*2);
-        startingGrains = startingGrains*2;
-        totalNumOfGrains = totalNumOfGrains + startingGrains
         countSquares++;
-    } return console.log(`The total number of grains is ${totalNumOfGrains}`
+        startingGrains = startingGrains*2;
+        console.log(`Square ${countSquares} equals ${startingGrains} grains.`);
+        totalNumOfGrains = totalNumOfGrains + startingGrains
+    } return console.log(`The total number of grains is ${totalNumOfGrains}.`)
 }
-
-grainCount(1,64)
+grainCount(1,64);
